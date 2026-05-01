939f91341fef77e49271f165f35c3448f8ba192f
Balnur <toksanbajbalnur@gmail.com>
2026-05-01 17:34:27 +0500

    fead: Role, DocumentStatus, ActionType тұрақтылар жиынын қосу

---
+++ src/main/java/com/example/project/enums/ActionType.java
@@ -0,0 +1,11 @@
+package com.example.project.enums;
+
+public enum ActionType {
+    UPLOAD,
+    VIEW,
+    APPROVE,
+    REJECT,
+    COMMENT,
+    DELETE,
+    UPDATE_STATUS
+}
---
+++ src/main/java/com/example/project/enums/DocumentStatus.java
@@ -0,0 +1,8 @@
+package com.example.project.enums;
+
+public enum DocumentStatus {
+    DRAFT,
+    IN_REVIEW,
+    APPROVED,
+    REJECTED
+}
