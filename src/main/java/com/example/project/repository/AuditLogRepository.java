1f1f54b6e52f3788ee1e23e8a36519cd9d745015
Balnur <toksanbajbalnur@gmail.com>
2026-05-01 18:18:52 +0500

    feat: AuditLogRepository қосу

---
+++ src/main/java/com/example/project/repository/AuditLogRepository.java
@@ -0,0 +1,10 @@
+package com.example.project.repository;
+
+import com.example.project.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findByUsername(String username);
+    List<AuditLog> findByDocumentId(Long documentId);
+}
